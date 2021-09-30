216c00e646a6d39ba1cc57ab60c0aa7c6a71c2ed
wild <52586172+wildwastaken@users.noreply.github.com>
2021-09-30 18:55:00 -0400

    Update faq.js

--- data/faq.js
+++ data/faq.js
@@ -2,14 +2,14 @@ export default [
   {
     question: "I'm new to coding. Can I still join?",
     answer: "Yes! Hack club is for people of all backgrounds and skill levels in CS. If you are a beginner, you can attend our workshops that we run every week that will walk you through the process of creating something awesome!"
-  },
+  },Fridays
   {
     question: "Do you guys actually hack?",
     answer: "No, that's not what we mean by \"hack\"! Hacking is a general term for taking advantage of unintended aspects of systems. We apply hacking to situations in life and we create apps and programs that can solve modern, real world issues."
   },
   {
     question: "When and where do meetings take place?",
-    answer: "For the 2021-22 school year, we meet on Fridays after school from 2:30 - 3:30 in Mr. Serapiglia's room (the Math wing). We'll also have a virtual option for those who can't make it in person. Join our Google Classroom for the link!"
+    answer: "For the 2021-22 school year, we meet on Tuesdays after school from 2:30 - 3:30 in Mr. Serapiglia's room (the Math wing). We'll also have a virtual option for those who can't make it in person. Join our Google Classroom for the link!"
   },
   {
     question: "Does it cost money to join?",
