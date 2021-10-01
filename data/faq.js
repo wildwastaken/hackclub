3b1dde69df7ec70dafe110c8ce95c1a8080ac742
wild <52586172+wildwastaken@users.noreply.github.com>
2021-09-30 21:27:47 -0400

    Update faq.js

--- data/faq.js
+++ data/faq.js
@@ -2,7 +2,7 @@ export default [
   {
     question: "I'm new to coding. Can I still join?",
     answer: "Yes! Hack club is for people of all backgrounds and skill levels in CS. If you are a beginner, you can attend our workshops that we run every week that will walk you through the process of creating something awesome!"
-  },Fridays
+  },
   {
     question: "Do you guys actually hack?",
     answer: "No, that's not what we mean by \"hack\"! Hacking is a general term for taking advantage of unintended aspects of systems. We apply hacking to situations in life and we create apps and programs that can solve modern, real world issues."
