446c3cdb547377078aad84be0d3af55f554a0ec1
wild <52586172+wildwastaken@users.noreply.github.com>
2021-10-19 12:58:04 -0400

    Update maintenance.js

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -15,15 +15,15 @@ export default function Custom404() {
   return (
     <>
       <Head>
-        <title>404!{config.titleSuffix}</title>
+        <title>Today's Workshop!{config.titleSuffix}</title>
       </Head>
 
       <Flex minH="90vh" align="center" justify="center">
         <Box align="center">
-          <Heading as="h1" size="2xl" color="brand.red">Whoops! 🚧</Heading>
-          <Text fontSize="lg">Sorry, this page is still under construction. Please check back soon for an update!</Text>
-          <Link href="/" style={{ textDecoration: "none" }}>
-            <Button colorScheme="red" mt={4}>Go Home</Button>
+          <Heading as="h1" size="2xl" color="brand.red">Hack Club Workshops</Heading>
+          <Text fontSize="lg">Welcome to today's workshop! Click the link below.</Text>
+          <Link href="https://workshops.hackclub.com/personal_website/" style={{ textDecoration: "none" }}>
+            <Button colorScheme="red" mt={4}>Workshop</Button>
           </Link>
         </Box>
       </Flex>
