ccd76e543cc9b1fdae8bd1edc34ba459747d3c74
allen liu <52586172+wildwastaken@users.noreply.github.com>
2022-10-27 20:25:19 -0400

    Update team.js

--- data/team.js
+++ data/team.js
@@ -12,5 +12,11 @@ export default {
       image: "/images/team/allen.png",
       link: "mailto:liual23@amityschools.org"
     },
+    {
+      name: "Charles Chen",
+      description: "Secretary",
+      image: "/images/team/charliebrownchen.png",
+      link: "mailto:chench23@amityschools.org"
+    },
   ]
 }
